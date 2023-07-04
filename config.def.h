4799b75670b6c7212a4198c90bb63d8cff0c77d8
Beomus <89083094+beomusxyz@users.noreply.github.com>
2023-07-05 01:44:14 +1000

    change font and remove unnecessary files

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Berkeley Mono:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 9;
 
 /*
